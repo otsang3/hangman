12b265bed84a1c752093c25be4c14f6ecc8e8ddf
otsang3 <ohhtsang@gmail.com>
2020-09-19 13:19:45 +0100

    set up express

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import express from 'express';
+import path from 'path';
 import React from 'react';
 import GameWindow from './components/GameWindow';
 import Header from './components/Header';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.listen(port, () => console.log('Listening on Port'), port)
 
 function App() {
   return (
